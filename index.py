2758f37843ae43d2fa3d00ed8019ce59e85b5ab5
Manuel Alejandro C. <120736944+Miauzzz@users.noreply.github.com>
2025-03-19 19:58:17 -0300

    fix2 port

--- index.py
+++ index.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from gestionapi import summoner_bp, cache
+import os  # Importa el módulo os para usar variables de entorno
 
 app = Flask(__name__)
-port = os.getenv("PORT", 3000))
+port = int(os.getenv("PORT", 3000))  # Asegúrate de convertir el puerto a entero
 
 # Configuración de caché
 app.config['CACHE_TYPE'] = 'simple'
 cache.init_app(app)
 
+# Registra el blueprint
 app.register_blueprint(summoner_bp, url_prefix='/summoner')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)  # Usa debug=False en producción
